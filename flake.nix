000e9e2668a987bce215ceb9a4c4f2549e5273ec
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-16 02:55:11 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
   description = "A simple NixOS flake";
 
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+
+  nixConfig = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 
   inputs = {
