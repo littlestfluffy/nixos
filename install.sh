e50963abb6a2b21a871e30a29eeb7d35e23e8ef2
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 00:30:23 +0200

    test

--- install.sh
+++ install.sh
@@ -26,11 +26,14 @@ fi
 if [ -n "$DISK" ]; then
   echo "Wiping and partitioning $DISK..."
   wipefs -a "$DISK"
-  parted "$DISK" -- mklabel MBR
-  parted "$DISK" -- mkpart primary ext4 1MiB 512MiB
-  parted "$DISK" -- mkpart primary ext4 513MiB 100%
+  # Use proper parted label
+  parted "$DISK" --script mklabel msdos
+  parted "$DISK" --script mkpart primary ext4 1MiB 512MiB
+  parted "$DISK" --script mkpart primary ext4 513MiB 100%
+
   mkfs.ext4 "${DISK}1" -L NIXBOOT
   mkfs.ext4 "${DISK}2" -L NIXROOT
+
   mount "${DISK}2" /mnt
   mount --mkdir "${DISK}1" /mnt/boot
 fi
@@ -39,10 +42,6 @@ fi
 # Install NixOS via flake
 # -------------------------
 echo "Installing NixOS for hostname '$HOSTNAME'..."
-if [ -n "$DISK" ]; then
-  nixos-install --impure --no-root-passwd --flake "github:littlestfluffy/nixos#$HOSTNAME"
-else
-  nixos-install --impure --no-root-passwd --flake "github:littlestfluffy/nixos#$HOSTNAME"
-fi
+nixos-install --impure --no-root-passwd --flake "github:littlestfluffy/nixos#$HOSTNAME"
 
 echo "✅ NixOS installation complete! Reboot into your new system."
