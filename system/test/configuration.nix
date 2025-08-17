4cd86a8f7b08604746cbdf328cb8d20b22b51bc7
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 15:33:08 +0200

    test

--- system/test/configuration.nix
+++ system/test/configuration.nix
@@ -1,15 +1,17 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 { config, lib, pkgs, inputs, ... }:
 
-let
-  candidates = [
-    ./hardware-configuration.nix
-    /mnt/etc/nixos/hardware-configuration.nix
-    /etc/nixos/hardware-configuration.nix
-  ];
-  existing = builtins.filter builtins.pathExists candidates;
-in
 {
-  imports = [
+  imports = let
+    candidates = [
+      ./hardware-configuration.nix
+      /mnt/etc/nixos/hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
+    ];
+    existing = builtins.filter builtins.pathExists candidates;
+  in [
     (if existing == [] then null else builtins.head existing)
     ./..
     ./../../modules/pipewire.nix
@@ -23,3 +25,12 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 }
+  imports = let
+    candidates = [
+      ./hardware-configuration.nix
+      /mnt/etc/nixos/hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
+    ];
+  in (builtins.filter builtins.pathExists candidates) ++ [ ./.. ] ++ [
+    ./../../modules/pipewire.nix
+  ];
