48fef7502624517135cdbdef21278b4de362e0a0
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 15:33:24 +0200

    test

--- system/test/configuration.nix
+++ system/test/configuration.nix
@@ -9,13 +9,9 @@
       ./hardware-configuration.nix
       /mnt/etc/nixos/hardware-configuration.nix
       /etc/nixos/hardware-configuration.nix
-    ];
-    existing = builtins.filter builtins.pathExists candidates;
-  in [
-    (if existing == [] then null else builtins.head existing)
-    ./..
-    ./../../modules/pipewire.nix
-  ];
+    ]; existing = builtins.filter builtins.pathExists candidates; in (if existing == [] then [] else [ builtins.head existing ])
+    ++ [ ./.. ]
+    ++ [ ./../../modules/pipewire.nix ];
 
   services.qemuGuest.enable = true;
 
