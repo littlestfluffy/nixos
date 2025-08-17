d44d999fdc2dc0dcb69858361c181a007706d31b
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 16:30:13 +0200

    test

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#    home-manager = {
+#      url = "github:nix-community/home-manager/release";
+#      inputs.nixpkgs.follows = "nixpkgs";
+#    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
