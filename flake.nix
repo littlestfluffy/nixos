c82d7521853f90b972c7f310a2caecee7781dfef
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 23:51:34 +0200

    test

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          ./system/default.nix
           ./system/${hostname}/configuration.nix
           { networking.hostName = hostname; boot.loader.grub.device = disk; }
         ];
