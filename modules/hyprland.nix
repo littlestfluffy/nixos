79c194c27b682553fef0940650f84675c8029b83
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 17:00:04 +0200

    test

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,4 +4,15 @@
   programs.hyprland = {
     enable = true;
   };
+
+  environment.systemPackages = [
+    pkgs.alacritty
+  ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    font-awesome
+  ];
 }
