53a8963b4c287ea7a5138772f88a2121c42e903d
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-17 16:10:10 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,18 +1,8 @@
 {
   description = "A simple NixOS flake";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://hyprland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    hyprland.url = "github:hyprwm/Hyprland";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -3,6 +3,5 @@
 {
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 }
