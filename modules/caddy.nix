fbec149c83ba4ffb1049a385da1b86551532ce0d
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 16:01:45 +0200

    caddy

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -10,8 +10,8 @@
     package = pkgs.caddy.withPlugins {
         plugins = [
           "github.com/caddy-dns/cloudflare@v0.2.1"
-          "github.com/WeidiDeng/caddy-cloudflare-ip@f53b62aa13cb7ad79c8b47aacc3f2f03989b67e5"
-          "github.com/mholt/caddy-dynamicdns@b846b9e8fb83f52be540fb7876116f944e56d551"
+          "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
+          "github.com/mholt/caddy-dynamicdns@v0.0.0-20250430031602-b846b9e8fb83"
         ];
         hash = "sha256-vg/Reqd7dPqIpCHTmm5BNd/EV72I09ccAQ1y+5X0kUE=";
       };
