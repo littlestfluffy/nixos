f51a2c9900c4afe91f05db85083a6eee743a0a1f
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 14:05:13 +0200

    add podman (test)

--- modules/podman.nix
+++ modules/podman.nix
@@ -5,18 +5,18 @@
     enable = true;
     defaultNetwork.settings.dns_enabled = true;
   };
-
-  virtualisation.oci-containers.containers = {
-    hackagecompare = {
-      image = "chrissound/hackagecomparestats-webserver:latest";
-      ports = ["0.0.0.0:3010:3010"];
-      volumes = [
-        "/root/hackagecompare/packageStatistics.json:/root/hackagecompare/packageStatistics.json"
-      ];
-      cmd = [
-        "--base-url"
-        "\"/hackagecompare\""
-      ];
-    };
-  };
+#
+#  virtualisation.oci-containers.containers = {
+#    hackagecompare = {
+#      image = "chrissound/hackagecomparestats-webserver:latest";
+#      ports = ["0.0.0.0:3010:3010"];
+#      volumes = [
+#        "/root/hackagecompare/packageStatistics.json:/root/hackagecompare/packageStatistics.json"
+#      ];
+#      cmd = [
+#        "--base-url"
+#        "\"/hackagecompare\""
+#      ];
+#    };
+#  };
 }
