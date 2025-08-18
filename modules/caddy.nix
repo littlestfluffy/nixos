6bbcbc395caf86260b1200d03ab6619efae170b7
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 15:52:59 +0200

    caddy

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -7,5 +7,13 @@
     enable = true;
     configFile = "/etc/caddy/Caddyfile";
     environmentFile = "/run/secrets/caddy.env";
+    package = pkgs.caddy.withPlugins {
+        plugins = [
+          "github.com/caddy-dns/cloudflare"
+          "github.com/WeidiDeng/caddy-cloudflare-ip"
+          "github.com/mholt/caddy-dynamicdns"
+        ];
+        hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+      };
   };
 }
