2f050456db1862e0045f20621a24fa84a5dc7b06
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 18:03:04 +0200

    use iptables (for docker)..

--- modules/docker-rootless.nix
+++ modules/docker-rootless.nix
@@ -1,6 +1,9 @@
 { inputs, pkgs, ... }:
 
 {
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+#  boot.kernel.sysctl."net.ipv6.ip_forward" = 1;
+
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
