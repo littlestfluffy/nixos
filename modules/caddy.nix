7f9f1504799fe7ed5800cddaa27d1481f315771f
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 15:58:54 +0200

    caddy

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -13,7 +13,7 @@
           "github.com/WeidiDeng/caddy-cloudflare-ip@f53b62aa13cb7ad79c8b47aacc3f2f03989b67e5"
           "github.com/mholt/caddy-dynamicdns@b846b9e8fb83f52be540fb7876116f944e56d551"
         ];
-        hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+        hash = "sha256-vg/Reqd7dPqIpCHTmm5BNd/EV72I09ccAQ1y+5X0kUE=";
       };
   };
 }
