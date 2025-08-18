fdda92f517588d75de1fb1322958402622f53724
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 22:20:52 +0200

    wireguard wg0

--- modules/network.nix
+++ modules/network.nix
@@ -3,6 +3,7 @@
 {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
+  networking.useDHCP = lib.mkDefault true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
