dccbec3c5f4cec88524e703d4d24cc383a96eeb4
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-18 18:00:27 +0200

    use iptables (for docker)..

--- modules/network.nix
+++ modules/network.nix
@@ -4,8 +4,6 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
 
-  networking.nftables.enable = true;
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
