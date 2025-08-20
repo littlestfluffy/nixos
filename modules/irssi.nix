324c6ff724752b429410ead2decbb9cd545827bf
LittlestFluffy <363388+littlestfluffy@users.noreply.github.com>
2025-08-20 02:11:20 +0200

    test

--- modules/irssi.nix
+++ modules/irssi.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  systemd.services.irssi = {
-    wantedBy = [ "multi-user.target" ];
+  systemd.user.services.irssi = {
+    wantedBy = [ "default.target" ];
     after = [ "network.target" ];
     description = "Start the irssi client of username.";
     serviceConfig = {
-      Type = "forking";
-      User = "${config.home.username}";
       ExecStart = ''
         ${pkgs.tmux}/bin/tmux new-session -d -s irssi ${pkgs.irssi}/bin/irssi
       '';
