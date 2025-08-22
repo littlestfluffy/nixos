18fc678a268c3467781cdc733e906215154e6a08
LittlestFluffy <littlestfluffy@github.com>
2025-08-22 19:35:03 +0200

    various fixes for desktop

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,14 +5,6 @@
     kernelParams = ["ipv6.disable=1"];
     tmp.cleanOnBoot = true;
     loader = {
-      grub = {
-        enable = true;
-        # device is (dynamically) set in flake.nix
-        useOSProber = true;
-      };
-    };
-
-    loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
