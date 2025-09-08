19a016a8f24a38358f3890139d79419a407deb1e
LittlestFluffy <littlestfluffy@github.com>
2025-09-08 12:12:26 +0200

    add pkgs.n8n

--- modules/docker-n8n.nix
+++ modules/docker-n8n.nix
@@ -5,10 +5,6 @@
     ./docker.nix
   ];
 
-  systemd.tmpfiles.rules = [
-    "d /var/lib/n8n 0755 1000 1000 -"
-  ];
-
   networking.firewall.allowedTCPPorts = [ 5678 ];
 
   virtualisation.oci-containers = {
@@ -17,7 +13,7 @@
       image = "docker.n8n.io/n8nio/n8n:latest";
       ports = [ "5678:5678" ];
       volumes = [
-        "/var/lib/n8n:/home/node/.n8n"
+        "n8n_data:/home/node/.n8n"
       ];
       environment = {
         N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS = "true";
