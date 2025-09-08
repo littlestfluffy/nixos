784bbae22ee50fa882561fa8592dd8d5610831c2
LittlestFluffy <littlestfluffy@github.com>
2025-09-08 12:02:02 +0200

    add pkgs.n8n

--- modules/docker-n8n.nix
+++ modules/docker-n8n.nix
@@ -1,19 +1,20 @@
-{config, pkgs, lib, utils, ...}:
+{ config, pkgs, lib, utils, ... }:
 
 {
-	imports = [
-		./docker.nix
-	];
+  imports = [
+    ./docker.nix
+  ];
 
   networking.firewall.allowedTCPPorts = [ 5678 ];
 
-  virtualisation.docker.enable = true;
-
-  virtualisation.oci-containers.containers.n8n = {
-    image = "docker.n8n.io/n8nio/n8n:latest";
-    ports = [ "5678:5678" ];
-    volumes = [
-      "n8n_data:/var/lib/n8n"
-    ];
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.n8n = {
+      image = "docker.n8n.io/n8nio/n8n:latest";
+      ports = [ "5678:5678" ];
+      volumes = [
+        "/var/lib/n8n:/home/node/.n8n"
+      ];
+    };
   };
 }
