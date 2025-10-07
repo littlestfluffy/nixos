b78374ad288647d33e91177a53fbd188d0131d67
LittlestFluffy <littlestfluffy@github.com>
2025-10-07 23:42:23 +0200

    add nextcloud (test)

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,6 +1,12 @@
 {config, pkgs, lib, utils, ...}:
 
 {
+  imports = [
+    "${fetchTarball {
+      url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+      sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
+  ];
+
   environment.etc."nextcloud-admin-pass".text = "PWD";
 
   networking.firewall.allowedTCPPorts = [ 80 ];
@@ -12,6 +18,16 @@
   services.nextcloud = {
     enable = true;
     configureRedis = true;
+    ensureUsers = {
+      user1 = {
+        email = "user1@localhost";
+        passwordFile = "/etc/nextcloud-admin-pass";
+      };
+      user2 = {
+        email = "user2@localhost";
+        passwordFile = "/etc/nextcloud-admin-pass";
+      };
+    };
     maxUploadSize = "10G";
     hostName = "192.168.2.67";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
@@ -38,27 +54,6 @@
         "OC\\Preview\\XBitmap"
         "OC\\Preview\\HEIC"
       ];
-    };
-  };
-
-  imports = [
-    "${fetchTarball {
-      url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
-      sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
-  ];
-
-  environment.etc."nextcloud-user-pass".text = "PWD";
-
-  services.nextcloud = {
-    ensureUsers = {
-      user1 = {
-        email = "user1@localhost";
-        passwordFile = "/etc/nextcloud-user-pass";
-      };
-      user2 = {
-        email = "user2@localhost";
-        passwordFile = "/etc/nextcloud-user-pass";
-      };
     };
   };
 }
