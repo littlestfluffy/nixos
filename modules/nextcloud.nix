9813583046355dc0a63d770f3381bd815a85c546
LittlestFluffy <littlestfluffy@github.com>
2025-10-07 23:38:11 +0200

    add nextcloud (test)

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,14 +1,7 @@
 {config, pkgs, lib, utils, ...}:
 
 {
-  imports = [
-    "${fetchTarball {
-      url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
-      sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
-  ];
-
   environment.etc."nextcloud-admin-pass".text = "PWD";
-  environment.etc."nextcloud-user-pass".text = "PWD";
 
   networking.firewall.allowedTCPPorts = [ 80 ];
 
@@ -46,10 +39,24 @@
         "OC\\Preview\\HEIC"
       ];
     };
+  };
 
+  imports = [
+    "${fetchTarball {
+      url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+      sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
+  ];
+
+  environment.etc."nextcloud-user-pass".text = "PWD";
+
+  services.nextcloud = {
     ensureUsers = {
       user1 = {
         email = "user1@localhost";
+        passwordFile = "/etc/nextcloud-user-pass";
+      };
+      user2 = {
+        email = "user2@localhost";
         passwordFile = "/etc/nextcloud-user-pass";
       };
     };
