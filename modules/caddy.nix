bb84cb664e5b507da4076b136a80eaaa1678810b
LittlestFluffy <littlestfluffy@github.com>
2025-10-08 23:33:02 +0200

    caddy update

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -12,9 +12,9 @@
           "github.com/caddy-dns/cloudflare@v0.2.1"
           "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
           "github.com/mholt/caddy-dynamicdns@v0.0.0-20250430031602-b846b9e8fb83"
-          "github.com/mholt/caddy-l4@v0.0.0-20251001194302-2e3e6cf60b25"
+          "github.com/mholt/caddy-l4@v0.0.0-20250530154005-4d3c80e89c5f"
         ];
-        hash = "sha256-aOPQ876f0kD9QledN/awgzjOW/ePuZ9uvnEXRrFMyrk=";
+        hash = "sha256-vg/Reqd7dPqIpCHTmm5BNd/EV72I09ccAQ1y+5X0kUE=";
       };
   };
 }
