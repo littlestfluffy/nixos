f404d5f13cde49d0924c0e28c01265ccebd7799d
LittlestFluffy <littlestfluffy@github.com>
2025-10-18 23:15:41 +0200

    haproxy update

--- modules/haproxy.nix
+++ modules/haproxy.nix
@@ -20,16 +20,21 @@
 
       frontend talos_api
         bind *:50000
-        default_backend talos_cp
+        default_backend talos_t
 
       frontend kube_api
         bind *:6443
         default_backend talos_cp
 
       backend talos_cp
-        server cp1 192.168.2.240:6443 check
-        server cp2 192.168.2.241:6443 check
-        server cp3 192.168.2.242:6443 check
+        server cp1 192.168.2.233:6443 check
+        server cp2 192.168.2.234:6443 check
+        server cp3 192.168.2.235:6443 check
+
+      backend talos_t
+        server t1 192.168.2.233:50000 check
+        server t2 192.168.2.234:50000 check
+        server t3 192.168.2.235:50000 check
     '';
   };
 }
