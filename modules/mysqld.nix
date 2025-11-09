7bae021125d6d4cbad4af08d47fe97f6c9013839
LittlestFluffy <littlestfluffy@github.com>
2025-11-09 12:05:41 +0100

    ...

--- modules/mysqld.nix
+++ modules/mysqld.nix
@@ -7,9 +7,13 @@
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
-    configFile = ''
-      [mysqld]
-      bind-address = 0.0.0.0
-    '';
+    configFile = pkgs.writeText "my.cnf" ''
+       [mysqld]
+       datadir = /var/lib/mysql
+       bind-address = 0.0.0.0
+       port = 3336
+
+       !includedir /etc/mysql/conf.d/
+     '';
   };
 }
